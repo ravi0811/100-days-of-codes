76092a37cf9e4d53641d8755d037419412186a35
ravi0811 <rr957226@gmail.com>
2026-03-28 12:10:20 +0530

    Update paddle.py

--- Day-22/paddle.py
+++ Day-22/paddle.py
@@ -1,20 +1,18 @@
 from turtle import Turtle
-
 class Paddle(Turtle):
     def __init__(self,position):
         super().__init__()
         self.penup()
         self.color("white")
         self.shape("square")
-        self.shapesize(5,1)
+        self.shapesize(stretch_wid=5,stretch_len=1)
         self.goto(position)
 
     def go_up(self):
         new_y=self.ycor()+20
-        self.goto(x=self.xcor(),y=new_y)
+        self.goto(self.xcor(),new_y)
     def go_down(self):
         new_y=self.ycor()-20
-        self.goto(x=self.xcor(),y=new_y)
-
+        self.goto(self.xcor(),new_y)
 
 
